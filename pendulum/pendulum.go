c3c552378c8e3860d2bfb2f1c990ef8021da765c
Hayden McAlister <haydenmcalister49@gmail.com>
2023-03-22 00:54:25 +1300

    Expose Omega values so these can be set from frontend

--- pendulum/pendulum.go
+++ pendulum/pendulum.go
@@ -3,7 +3,6 @@ package pendulum
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"math"
 	"net/http"
 	"time"
@@ -14,23 +13,33 @@ import (
 )
 
 const (
-	G                 float64 = 9.81
+	G                 float64 = 10
 	TIME_DELTA        float64 = 0.005
-	FRICTION_CONSTANT float64 = 0.1
+	FRICTION_CONSTANT float64 = 0.05
 )
 
 type PendulumState struct {
 	L1     float64
 	M1     float64
 	Theta1 float64
-	omega1 float64
+	Omega1 float64
 
 	L2     float64
 	M2     float64
 	Theta2 float64
-	omega2 float64
+	Omega2 float64
 
 	time float64
+}
+
+func normalizeAngle(angle float64) float64 {
+	for angle > math.Pi {
+		angle -= 2 * math.Pi
+	}
+	for angle < (-math.Pi) {
+		angle += 2 * math.Pi
+	}
+	return angle
 }
 
 func NewRandomState() *PendulumState {
@@ -49,12 +58,12 @@ func NewRandomState() *PendulumState {
 		L1:     unitUniform.Rand(),
 		M1:     unitUniform.Rand(),
 		Theta1: angleUniform.Rand(),
-		omega1: 0.0,
+		Omega1: 0.0,
 
 		L2:     unitUniform.Rand(),
 		M2:     unitUniform.Rand(),
 		Theta2: angleUniform.Rand(),
-		omega2: 0.0,
+		Omega2: 0.0,
 
 		time: 0.0,
 	}
@@ -64,31 +73,31 @@ func NewRandomState() *PendulumState {
 func (p *PendulumState) NextState() {
 	omega1dot := -G*(2.0*p.M1+p.M2)*math.Sin(p.Theta1) +
 		-G*p.M2*math.Sin(p.Theta1-2.0*p.Theta2) +
-		-2.0*math.Sin(p.Theta1-p.Theta2)*p.M2*(math.Pow(p.omega2, 2)*p.L2+math.Pow(p.omega1, 2)*p.L1*math.Cos(p.Theta1-p.Theta2))
+		-2.0*math.Sin(p.Theta1-p.Theta2)*p.M2*(math.Pow(p.Omega2, 2)*p.L2+math.Pow(p.Omega1, 2)*p.L1*math.Cos(p.Theta1-p.Theta2))
 	omega1dot /= p.L1 * (2.0*p.M1 + p.M2 - p.M2*math.Cos(2*p.Theta1-2*p.Theta2))
-	omega1dot -= FRICTION_CONSTANT * p.omega1
+	omega1dot -= FRICTION_CONSTANT * p.Omega1
 
-	omega2dot := math.Pow(p.omega1, 2)*p.L1*(p.M1+p.M2) +
+	omega2dot := math.Pow(p.Omega1, 2)*p.L1*(p.M1+p.M2) +
 		G*(p.M1+p.M2)*math.Cos(p.Theta1) +
-		math.Pow(p.omega2, 2)*p.L2*p.M2*math.Cos(p.Theta1-p.Theta2)
+		math.Pow(p.Omega2, 2)*p.L2*p.M2*math.Cos(p.Theta1-p.Theta2)
 	omega2dot *= 2 * math.Sin(p.Theta1-p.Theta2)
 	omega2dot /= p.L2 * (2.0*p.M1 + p.M2 - p.M2*math.Cos(2*p.Theta1-2*p.Theta2))
-	omega2dot -= FRICTION_CONSTANT * p.omega2
+	omega2dot -= FRICTION_CONSTANT * p.Omega2
 
-	nextOmega1 := p.omega1 + omega1dot*TIME_DELTA
-	nextOmega2 := p.omega2 + omega2dot*TIME_DELTA
+	nextOmega1 := p.Omega1 + omega1dot*TIME_DELTA
+	nextOmega2 := p.Omega2 + omega2dot*TIME_DELTA
 
-	nextTheta1 := p.Theta1 + nextOmega1*TIME_DELTA
-	nextTheta2 := p.Theta2 + nextOmega2*TIME_DELTA
+	nextTheta1 := normalizeAngle(p.Theta1 + nextOmega1*TIME_DELTA)
+	nextTheta2 := normalizeAngle(p.Theta2 + nextOmega2*TIME_DELTA)
 
 	// fmt.Printf("%#v, %#v\n", omega1dot, omega2dot)
 	// fmt.Printf("%#v, %#v\n", p.omega1-nextOmega1, p.omega2-nextOmega2)
 	// fmt.Printf("%#v, %#v\n", p.Theta1-nextTheta1, p.Theta2-nextTheta2)
 
 	p.Theta1 = nextTheta1
-	p.omega1 = nextOmega1
+	p.Omega1 = nextOmega1
 	p.Theta2 = nextTheta2
-	p.omega2 = nextOmega2
+	p.Omega2 = nextOmega2
 	p.time = p.time + TIME_DELTA
 }
 
@@ -109,17 +118,16 @@ func (p *PendulumState) SetState(w http.ResponseWriter, req *http.Request) {
 	if err := json.NewDecoder(req.Body).Decode(&newState); err != nil {
 		fmt.Printf("ERROR while decoding POST request body! %#v\n", err)
 	}
-	fmt.Printf("New State from Client: %#v\n", newState)
 
 	p.L1 = newState.UpperLength
 	p.M1 = newState.UpperMass
 	p.Theta1 = newState.UpperAngle
-	p.omega1 = newState.UpperAngularVelocity
+	p.Omega1 = newState.UpperAngularVelocity
 
 	p.L2 = newState.LowerLength
 	p.M2 = newState.LowerMass
 	p.Theta2 = newState.LowerAngle
-	p.omega2 = newState.LowerAngularVelocity
+	p.Omega2 = newState.LowerAngularVelocity
 
 	p.time = 0
 }
@@ -139,14 +147,12 @@ func (p *PendulumState) Randomize(w http.ResponseWriter, req *http.Request) {
 	p.L1 = randState.L1
 	p.M1 = randState.M1
 	p.Theta1 = randState.Theta1
-	p.omega1 = randState.omega1
+	p.Omega1 = randState.Omega1
 
 	p.L2 = randState.L2
 	p.M2 = randState.M2
 	p.Theta2 = randState.Theta2
-	p.omega2 = randState.omega2
+	p.Omega2 = randState.Omega2
 
 	p.time = randState.time
-
-	log.Printf("STATE RANDOMIZED: %#v\n", p)
 }
